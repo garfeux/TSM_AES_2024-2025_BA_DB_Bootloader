321922abda7c55d26b06950782c80416f1927971
Dupertuis Baptiste <baptiste.dupertui@he-arc.ch>
2025-01-03 16:10:44 +0100

    bootloader app to Intermediate Wrap-up state

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "mbed.h"
 
 #include "mbed_trace.h"
+#include "FlashIAPBlockDevice.h"
+#include "block_device_application.hpp"
+
 #if MBED_CONF_MBED_TRACE_ENABLE
 #define TRACE_GROUP "bootloader"
 #endif // MBED_CONF_MBED_TRACE_ENABLE
@@ -26,12 +29,65 @@ int main()
 
     tr_debug("BikeComputer bootloader\r\n");
 
-    // at this stage we directly branch to the main application
-    void *sp = *((void **) POST_APPLICATION_ADDR + 0);  // NOLINT(readability/casting)
-    void *pc = *((void **) POST_APPLICATION_ADDR + 1);  // NOLINT(readability/casting)
-    tr_debug("Starting application at address 0x%08x (sp 0x%08x, pc 0x%08x)\r\n", POST_APPLICATION_ADDR, (uint32_t) sp, (uint32_t) pc);
+    FlashIAPBlockDevice flashIAPBlockDevice(MBED_ROM_START, MBED_ROM_SIZE);
 
-    mbed_start_application(POST_APPLICATION_ADDR);
+    if(flashIAPBlockDevice.init() == 0)
+    {    
+        // addresses are specified relative to block device base address
+        mbed::bd_addr_t headerAddress      = HEADER_ADDR - MBED_ROM_START;        
+        mbed::bd_addr_t applicationAddress = POST_APPLICATION_ADDR - MBED_ROM_START;
+
+        update_client::BlockDeviceApplication activeApplication = update_client::BlockDeviceApplication(flashIAPBlockDevice, headerAddress, applicationAddress);
+        update_client::UCErrorCode status = activeApplication.checkApplication();
+
+        switch (status) {
+        case update_client::UCErrorCode::UC_ERR_NONE: 
+            tr_debug("SUCCESS: Application is valid.\r\n");
+
+
+
+            break;
+        case update_client::UCErrorCode::UC_ERR_INVALID_HEADER:
+            tr_debug("ERROR: Invalid Header.\r\n");
+            break;
+        case update_client::UCErrorCode::UC_ERR_INVALID_CHECKSUM:
+            tr_debug("ERROR: Invalid checksum - Application is corrupted\r\n");
+            break;
+        case update_client::UCErrorCode::UC_ERR_READ_FAILED:
+            tr_debug("ERROR: Read application failed.\r\n");
+            break;
+        case update_client::UCErrorCode::UC_ERR_HASH_INVALID:
+            tr_debug("ERROR: Invalid hash - Application is corrupted\r\n");
+            break;
+        case update_client::UCErrorCode::UC_ERR_FIRMWARE_EMPTY:
+            tr_debug("ERROR: The application is empty.\r\n");
+            break;
+        case update_client::UCErrorCode::UC_ERR_PROGRAM_FAILED:
+            tr_debug("ERROR: Program failed.\r\n");
+            break;
+        case update_client::UCErrorCode::UC_ERR_CANNOT_INIT:
+            tr_debug("ERROR: Cannot init.\r\n");
+            break;
+        case update_client::UCErrorCode::UC_ERR_CANNOT_START_THREAD:
+            tr_debug("ERROR: Cannot start thread.\r\n");
+            break;
+        default:
+            tr_debug("ERROR: Unknown error.\r\n");
+            break;
+        }
+
+        if(status == update_client::UCErrorCode::UC_ERR_NONE){
+            // at this stage we directly branch to the main application
+            void *sp = *((void **) POST_APPLICATION_ADDR + 0);  // NOLINT(readability/casting)
+            void *pc = *((void **) POST_APPLICATION_ADDR + 1);  // NOLINT(readability/casting)
+            tr_debug("Starting application at address 0x%08x (sp 0x%08x, pc 0x%08x)\r\n", POST_APPLICATION_ADDR, (uint32_t) sp, (uint32_t) pc);
+
+            mbed_start_application(POST_APPLICATION_ADDR);
+        }
+
+    } else {
+        tr_debug("ERROR: flashIAPBlockDevice init failed.\r\n");
+    }
 
     return 0;
 }
